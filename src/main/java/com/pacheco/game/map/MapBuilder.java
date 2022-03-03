4d7395db5bbcc98a19aac5f43b567842382348b2
Vinicius Rodrigues <vinicius.rodrigues.lima@ccc.ufcg.edu.br>
2022-03-03 09:14:33 -0300

    add tile factory

--- src/main/java/com/pacheco/game/map/MapBuilder.java
+++ src/main/java/com/pacheco/game/map/MapBuilder.java
@@ -1,65 +1,55 @@
 package com.pacheco.game.map;
 
 import com.pacheco.game.SpritesHolder;
-import com.pacheco.game.component.*;
-import com.pacheco.game.entity.Entity;
 import com.pacheco.game.entity.EntityPool;
-import javafx.scene.paint.Color;
 
 public class MapBuilder {
 
     private static final String mapString = "" +
-            "xxxxx.........xxxxx.....xxxx\n" +
-            "xxxxx.........xxxxx........x\n" +
-            "x..........................x\n" +
-            "x..........................x\n" +
-            "x..........................\n" +
-            "x..........................\n" +
-            "x..........................\n" +
-            "x..........................\n" +
-            "x..........................\n" +
-            "x..........................\n" +
-            "x..........................\n" +
-            "x..........................\n" +
-            "x..........................\n" +
-            "x..........................\n" +
-            "x..........................\n" +
-            "x..........................\n" +
-            "x..........................\n" +
-            "xxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+            "wwwww.........wwwww.....wwww\n" +
+            "wwwww.........wwwww........w\n" +
+            "w..........................w\n" +
+            "w..................r.......w\n" +
+            "w.....r....................\n" +
+            "w..........................\n" +
+            "w..........................\n" +
+            "w.........wwww.............\n" +
+            "w..........................\n" +
+            "w....rr....................\n" +
+            "w..........................\n" +
+            "w.........rrrrr............\n" +
+            "w..........................\n" +
+            "w..........................\n" +
+            "w.....r............r.......\n" +
+            "w..........................\n" +
+            "w..........................\n" +
+            "wwwwwwwwwwwwwwwwwwwwwwwwwww";
 
-    public void build(EntityPool entityPool) {
+    private TileFactory tileFactory;
+
+    public MapBuilder() {
         int cellW = 50;
         int cellH = 50;
         double offsetW = -300;
         double offsetH = -300;
+        tileFactory = new TileFactory(offsetW, offsetH, cellW, cellH);
+    }
+
+    public void build(EntityPool entityPool) {
         int column = 0;
         int line = 0;
         for (int i = 0; i < mapString.length(); i++) {
-            switch (mapString.charAt(i)) {
-                case 'x': {
-                    Entity wall = new Entity();
-                    wall.setComponent(PositionComponent.class, new PositionComponent(cellW * column, cellH * line));
-                    wall.setComponent(BoundingBoxComponent.class,
-                            new BoundingBoxComponent(0, 0, cellW, cellH, BoundingBoxType.SOLID));
-                    wall.setComponent(GraphicComponent.class, new TileGraphicComponent(SpritesHolder.WOOD_BLOCK, cellW, cellH));
-                    entityPool.addEntity(wall);
-                    break;
-                }
-                case '.': {
-                    Entity grass = new Entity();
-                    grass.setComponent(PositionComponent.class, new PositionComponent(cellW * column, cellH * line));
-                    grass.setComponent(GraphicComponent.class, new TileGraphicComponent(SpritesHolder.GRASS, cellW, cellH));
-                    entityPool.addEntity(grass);
-                    break;
-                }
-                case '\n': {
-                    line++;
-                    column = -1;
-                    break;
-                }
+            char c = mapString.charAt(i);
+            if (c == 'w') entityPool.addEntity(tileFactory.createSolid(SpritesHolder.WOOD_BLOCK, column, line));
+            if (c == 'r') entityPool.addEntity(tileFactory.createSolid(SpritesHolder.ROCK_BLOCK, column, line));
+            if (c == '.') entityPool.addEntity(tileFactory.createSurface(SpritesHolder.GRASS, column, line));
+
+            if (c == '\n') {
+                line++;
+                column = 0;
+            } else {
+                column++;
             }
-            column++;
         }
     }
 }
---
+++ src/main/java/com/pacheco/game/map/TileFactory.java
@@ -0,0 +1,38 @@
+package com.pacheco.game.map;
+
+import com.pacheco.game.SpritesHolder;
+import com.pacheco.game.component.*;
+import com.pacheco.game.entity.Entity;
+import javafx.scene.image.Image;
+
+public class TileFactory {
+
+    private double initialX;
+    private double initialY;
+    private double tileW;
+    private double tileH;
+
+    public TileFactory(double initialX, double initialY, double tileW, double tileH) {
+        this.initialX = initialX;
+        this.initialY = initialY;
+        this.tileW = tileW;
+        this.tileH = tileH;
+    }
+
+    public Entity createSolid(Image sprite, int column, int line) {
+        Entity solid = new Entity();
+        solid.setComponent(PositionComponent.class, new PositionComponent(tileW * column, tileH * line));
+        solid.setComponent(BoundingBoxComponent.class,
+                new BoundingBoxComponent(0, 0, tileW, tileH, BoundingBoxType.SOLID));
+        solid.setComponent(
+                GraphicComponent.class, new TileGraphicComponent(sprite, tileW, tileH));
+        return solid;
+    }
+
+    public Entity createSurface(Image sprite, int column, int line) {
+        Entity surface = new Entity();
+        surface.setComponent(PositionComponent.class, new PositionComponent(tileW * column, tileH * line));
+        surface.setComponent(GraphicComponent.class, new TileGraphicComponent(sprite, tileW, tileH));
+        return surface;
+    }
+}
